e6def6cc906a8352cb032195dd3f136fd29fba4a
SDX24 <161406791+SDX24@users.noreply.github.com>
2025-09-18 21:08:05 -0700

    setup next project

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <header className="flex flex-col justify-center items-center">
+        <h1>
+          Tandem
+        </h1>
+        <p>
+          Project frontpage
+        </p>
+      </header>
+
+      <main className="min-h-screen flex flex-col justify-center items-center text-center">
+        <Link href="/test" className="text-blue-500 hover:text-blue-700 underline">
+          Test Page
+        </Link>
+      </main>
+    </div>
+  );
+}
---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function Test() {
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <p>This is the test page</p>
+      <p>route is localhost:3000/test</p>
+    </div>
+  );
+}
