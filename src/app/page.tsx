e8ceb4b230ce88bf20cdc71b9db0648ae3c94469
SDX24 <161406791+SDX24@users.noreply.github.com>
2025-10-08 13:47:36 -0700

    daycare searcher working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,10 +16,6 @@ export default function Home() {
         <Link href="/test" className="text-blue-500 hover:text-blue-700 underline">
           Test Page
         </Link>
-        
-        <Link href="/testApi" className="text-blue-500 hover:text-blue-700 underline">
-          Api usage example
-        </Link>
 
           <Link href="/map" className="text-blue-500 hover:text-blue-700 underline">
           Google maps
