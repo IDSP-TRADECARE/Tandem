535b16bd564fb8fc4da8891824ea944b3d68b4ef
SDX24 <161406791+SDX24@users.noreply.github.com>
2025-11-18 11:59:28 -0800

    add temp for outline

--- src/app/components/ui/calendar/DateCard.tsx
+++ src/app/components/ui/calendar/DateCard.tsx
@@ -8,9 +8,10 @@ interface DateCardProps {
   onClick?: () => void;
   isEmpty?: boolean;
   isWork?: boolean;
+  date?: string;
 }
 
-export function DateCard({ text = "EPMTY CARD", onClick, isEmpty, isWork, type }: DateCardProps) {
+export function DateCard({ text = "EPMTY CARD", date, onClick, isEmpty, isWork, type }: DateCardProps) {
     let barColor = '#b0b0b8';
     if (!isEmpty) {
         barColor = isWork? '#6bb064' : "#255495";
@@ -21,24 +22,27 @@ export function DateCard({ text = "EPMTY CARD", onClick, isEmpty, isWork, type }
     const fontBoldness = type === 'Today' ? 'font-medium' : '';
     
   return (
-    <button
-      onClick={onClick}
-      className="relative bg-white rounded-3xl shadow-lg px-8 py-8 flex items-center justify-between w-full h-16 hover:shadow-xl transition-shadow overflow-hidden"
-    >
-      <div 
-        className="absolute left-0 top-0 bottom-0 w-3 rounded-l-3xl"
-        style={{ backgroundColor: barColor }}
-      />
-      {isEmpty ? (
-        <h2 className={`${textSize} ${fontBoldness} text-black ${typographyClass}`}>{text}</h2>
-      ) : (
-        <div className="flex flex-col items-start">
-          <p className={`text-[14px] text-black ${typography.body.body}`}>Additional info</p>
+    <div className="flex items-center gap-4">
+      <p>{date || "null"}</p>
+      <button
+        onClick={onClick}
+        className="relative bg-white rounded-3xl shadow-lg px-8 py-8 flex items-center justify-between w-full h-16 hover:shadow-xl transition-shadow overflow-hidden"
+      >
+        <div 
+          className="absolute left-0 top-0 bottom-0 w-3 rounded-l-3xl"
+          style={{ backgroundColor: barColor }}
+        />
+        {isEmpty ? (
           <h2 className={`${textSize} ${fontBoldness} text-black ${typographyClass}`}>{text}</h2>
-        </div>
-      )}
-      <IoIosArrowForward size={28} color="#b0b0b8" className="z-10" />
-    </button>
+        ) : (
+          <div className="flex flex-col items-start">
+            <p className={`text-[14px] text-black ${typography.body.body}`}>Additional info</p>
+            <h2 className={`${textSize} ${fontBoldness} text-black ${typographyClass}`}>{text}</h2>
+          </div>
+        )}
+        <IoIosArrowForward size={28} color="#b0b0b8" className="z-10" />
+      </button>
+    </div>
   );
 }
 
