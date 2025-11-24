f5a0d11431551f26f247e76a966ad85d9739c2b4
Sandy Chow <sandychow@Sandys-MacBook-Pro.local>
2025-11-23 17:01:42 -0800

    adjust css for Help Centre page

--- src/app/profile/HelpCentre/page.tsx
+++ src/app/profile/HelpCentre/page.tsx
@@ -17,22 +17,24 @@ function HelpCentre() {
             <ProfileHeader title="Help Centre" />
         </div> 
       <HalfBackground topPosition="140px">
-        <div className="p-8 space-y-8">
+        <div className="p-8 space-y-8 pl-6">
           <h3 className="text-xl font-bold font-alan">Feature Guide</h3>
           <div className="ml-0.5 mr-0.5">
             <ProfileCardCarousel />
           </div>
-          <OptionButton
-            icon="/profile/ComponentIcon/Privacy.svg"
-            text="Privacy & Security"
-            onClick={() => router.push('/profile/Privacy')}
-          />
-          <ActionButton
-          className="font-medium font-alan"
-          text="Contact Us"
-          icon="/profile/ComponentIcon/Text Bubble.svg"
-          onClick={() => router.push('/profile/ContactUs')}
-        />
+          <div className="flex flex-col items-center space-y-8">
+            <OptionButton
+              icon="/profile/ComponentIcon/Privacy.svg"
+              text="Privacy & Security"
+              onClick={() => router.push('/profile/Privacy')}
+            />
+            <ActionButton
+              className="font-medium font-alan"
+              text="Contact Us"
+              icon="/profile/ComponentIcon/Text Bubble.svg"
+              onClick={() => router.push('/profile/ContactUs')}
+            />
+          </div>
         </div>
         <BottomNav />
     </HalfBackground>
